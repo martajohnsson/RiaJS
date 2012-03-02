b731d1cf8b7c1fdab65cd909adfdb3895409a8de
Rickard <zupher@gmail.com>
2012-03-02 11:53:38 +0100

    fixed validation on input, must be < 0 and a number

--- application/internal/javascript/views/view_start.js
+++ application/internal/javascript/views/view_start.js
@@ -1,20 +1,20 @@
-// BEROENDEN
+﻿// BEROENDEN
 // ============================================================
 // external/javascript/libs/backbone/backbone.js
 // internal/javascript/views/view_budget_post.js
 // internal/javascript/collections/collection_budget_posts.js
 // internal/javascript/models/model_budget_post.js
 
-// NOTE: Egentligen skulle denna fil vara uppdelad i tre klasser (för formulär, diagram och tabell)
-// men pågrund av tidsbrist så skippas det för den här gången. Filerna (tomma!) ligger dock i
+// NOTE: Egentligen skulle denna fil vara uppdelad i tre klasser (fÃ¶r formulÃ¤r, diagram och tabell)
+// men pÃ¥grund av tidsbrist sÃ¥ skippas det fÃ¶r den hÃ¤r gÃ¥ngen. Filerna (tomma!) ligger dock i
 // katalogen unused
 
 
-// Vy-klassen som ritar ut det huvudsakliga gränssnittet
+// Vy-klassen som ritar ut det huvudsakliga grÃ¤nssnittet
 var view_start = Backbone.View.extend
 ({
-	// Använd den enda mallen som finns i applikationen, hämta in de lagrade modellerna
-	// samt bind händelser för ändring/borttagning av modeller till metoder i den här klassen
+	// AnvÃ¤nd den enda mallen som finns i applikationen, hÃ¤mta in de lagrade modellerna
+	// samt bind hÃ¤ndelser fÃ¶r Ã¤ndring/borttagning av modeller till metoder i den hÃ¤r klassen
 	initialize: function(a_placeholder, a_collection)
 	{
 		this.template = _.template($('#start').html());
@@ -23,8 +23,8 @@ var view_start = Backbone.View.extend
 		this.collection_budget_posts.on('change', this.render, this);
 	},
 
-	// Lyssna till händelser i denna klass och kör metoder
-	// baserat på musklick osv
+	// Lyssna till hÃ¤ndelser i denna klass och kÃ¶r metoder
+	// baserat pÃ¥ musklick osv
 	events: {
     	'click #send_post': 'addBudgetPost',
   	},
@@ -37,17 +37,17 @@ var view_start = Backbone.View.extend
 		this.fillDiagram();
 	},
 
-	// Lägg till en budgetpost genom att hämta kategori (kategori och typ) samt
-	// värde från gränssnittet
+	// LÃ¤gg till en budgetpost genom att hÃ¤mta kategori (kategori och typ) samt
+	// vÃ¤rde frÃ¥n grÃ¤nssnittet
 	addBudgetPost: function()
 	{
-		// Hämta kategorin från den text du ser i gränssnittet för <select>-elementet
+		// HÃ¤mta kategorin frÃ¥n den text du ser i grÃ¤nssnittet fÃ¶r <select>-elementet
 		this.select_category = $('#select_category :selected').text();
 
-		// Hämta typen (inkomst/utgift) från value-värdet för den markerade raden i <select>-elementet
+		// HÃ¤mta typen (inkomst/utgift) frÃ¥n value-vÃ¤rdet fÃ¶r den markerade raden i <select>-elementet
 		this.select_type = $('#select_category').val();
 
-		// Hämta värdet i siffror från det andra fältet
+		// HÃ¤mta vÃ¤rdet i siffror frÃ¥n det andra fÃ¤ltet
 		this.input_value = $('#input_value').val();
 
 		if (this.select_category != '' && this.input_value != '')
@@ -59,16 +59,21 @@ var view_start = Backbone.View.extend
 				type: this.select_type,
 				value: this.input_value
 			});
-
-			//Lägg till en ny modell till lagringen (localstorage)
-			this.collection_budget_posts.add(model);
-
-			// Spara modellen tills den tas bort/rensas etc
-			model.save();
-	
-			// Rensa kategori och värde från synligt innehåll
-			this.$('#select_category').val('');
-			this.$('#input_value').val('');
+            
+            if ( model.isValid() ) {
+    			//LÃ¤gg till en ny modell till lagringen (localstorage)
+    			this.collection_budget_posts.add(model);
+    
+    			// Spara modellen tills den tas bort/rensas etc
+    			model.save();
+    			
+                // Rensa kategori och vÃ¤rde frÃ¥n synligt innehÃ¥ll
+                this.$('#select_category').val('');
+                this.$('#input_value').val('');
+            } else {
+                apprise('Du måste ange en summa som är större än 0!');
+                model.destroy();
+            }
 		}
 		else
 		{
@@ -82,7 +87,7 @@ var view_start = Backbone.View.extend
 		// Skriv ut inkomster och utgifter i ett cirkeldiagram
 		this.outputDiagram = new Bluff.Pie('income_outcome_graph', '600x300');
 
-		// Låt cirkeldiagrammet få ett speciellt tema med färger
+		// LÃ¥t cirkeldiagrammet fÃ¥ ett speciellt tema med fÃ¤rger
 		this.outputDiagram.set_theme
 		({
 	    	marker_color: '#000000',
@@ -90,17 +95,17 @@ var view_start = Backbone.View.extend
 	    	background_colors: ['#ffffff', '#ffffff']
 	  	});
 	
-		// Skriv ut inkomster i en grön färg
+		// Skriv ut inkomster i en grÃ¶n fÃ¤rg
 	    this.outputDiagram.data('Inkomster', [parseInt(a_incomes)], '#00ff00');
 
-		// Skriv ut utgifter i en röd färg
+		// Skriv ut utgifter i en rÃ¶d fÃ¤rg
 	    this.outputDiagram.data('Utgifter', [parseInt(a_outcomes)], '#ff0000');
 	
 		// Rita ut diagrammet
 	    this.outputDiagram.draw();
 	},
 
-	// Lägg till en budgetpost till gränssnittet
+	// LÃ¤gg till en budgetpost till grÃ¤nssnittet
 	addSingleItem: function(a_post)
 	{
 		$('#budgetItems').append(new view_budget_post({model: a_post}).el);
@@ -112,7 +117,7 @@ var view_start = Backbone.View.extend
 		incomes = 0;
 		outcomes = 0;
 
-		// Dölj titlarna för hur mycket pengar man har kvar osv
+		// DÃ¶lj titlarna fÃ¶r hur mycket pengar man har kvar osv
 		if (this.collection_budget_posts.length == 0)
 		{
 			this.$('#budgetOutput').hide();
@@ -122,22 +127,22 @@ var view_start = Backbone.View.extend
 			this.$('#budgetOutput').show();
 		}
 
-		// Räkna hur mycket inkomster och utgifter det finns
+		// RÃ¤kna hur mycket inkomster och utgifter det finns
 		this.collection_budget_posts.each(function(a_post)
 		{
 			post = a_post.toJSON();
 		
 			if (post.type == 'Inkomst')
 			{
-				incomes += parseInt(post.value); // Räkna totalt antal inkomster
+				incomes += parseInt(post.value); // RÃ¤kna totalt antal inkomster
 			}
 			else if (post.type == 'Utgift')
 			{
-				outcomes += parseInt(post.value); // Räkna totalt antal utgifter
+				outcomes += parseInt(post.value); // RÃ¤kna totalt antal utgifter
 			}
 		});
 
-		// Om det finns inkomster/utgifter, visa titlar för hur mycket pengar det finns kvar osv
+		// Om det finns inkomster/utgifter, visa titlar fÃ¶r hur mycket pengar det finns kvar osv
 		if (incomes != 0 || outcomes != 0)
 		{
 			$('#budgetSummaryIncomePlaceholder').show();
@@ -146,7 +151,7 @@ var view_start = Backbone.View.extend
 			$('#budgetSummaryOutcomePlaceholder').show();
 			$('#budgetSummaryOutcome').html(parseInt(outcomes) + ' kr');
 		}
-		// Om det inte finns inkomster/utgifter, dölj titlar för hur mycket pengar det finns kvar osv
+		// Om det inte finns inkomster/utgifter, dÃ¶lj titlar fÃ¶r hur mycket pengar det finns kvar osv
 		else
 		{
 			$('#budgetSummaryIncomePlaceholder').hide();
@@ -156,7 +161,7 @@ var view_start = Backbone.View.extend
 			$('#budgetSummaryOutcome').html('');
 		}
 
-		// Kalla på metoden som ritar ut diagrammet
+		// Kalla pÃ¥ metoden som ritar ut diagrammet
 		this.drawDiagram(incomes, outcomes);
 	}
 });
